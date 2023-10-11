3aed5f4ab3c7f425770b60e451ba4496364116c2
Juan Torres <junete190@gmail.com>
2023-10-11 09:27:17 -0500

    Avance maquetado disposición de la card

--- src/components/WeatherContainer.jsx
+++ src/components/WeatherContainer.jsx
@@ -1,25 +1,30 @@
-import "./WeatherContainer.css"
+import "./WeatherContainer.css";
 
 const WeatherContainer = ({ weather }) => {
   console.log(weather);
   return (
-    <section>
+    <section className="min">
       <h1>Weather app</h1>
 
-      <div className="main text-black">
-        <span className="temp">{weather.main.temp}°F</span>
-        <picture className="image">
-          <img src="/Frame.png" alt="" />
+      <div className="relative text-[#026EED]">
+        <picture>
+          <img src="/Rectangle.png" alt="" />
         </picture>
-        <div>
-          <p>Viento</p>
-          <p>Nubes</p>
-          <p>Presión</p>
+        <div className="absolute top-[20px] left-[20px] grid grid-cols-2 grid-rows-4 gap-x-[30px]">
+          <span className="temp text-[40px] flex items-end">{weather.main.temp}°F</span>
+          <picture className="image">
+            <img className="w-[140px] h-[140px]" src="/Frame.png" alt="" />
+          </picture>
+          <div className="data--container flex flex-col justify-end font-normal">
+            <p>Viento</p>
+            <p>Nubes</p>
+            <p>Presión</p>
+          </div>
+          <p className="desc--container flex items-center justify-end">{weather.weather[0].description}</p>
+          <h3 className="location--container flex items-center font-semibold">
+            {weather.name}, {weather.sys.country}
+          </h3>
         </div>
-        <p>{weather.weather[0].description}</p>
-        <h3>
-          {weather.name}, {weather.sys.country}
-        </h3>
       </div>
     </section>
   );

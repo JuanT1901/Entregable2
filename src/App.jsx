c1e8b17550b92ea59858297c5258b7b4053e370a
Juan Torres <junete190@gmail.com>
2024-02-24 21:39:36 -0500

    Avance corrección P2

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,68 @@ import WeatherContainer from "./components/WeatherContainer";
 import Loader from "./components/Loader";
 
 function App() {
-  const [weather, setWeather] = useState(null);
-  const [currentBg, setCurrentBg] = useState("bg1")
-
-  const success = (pos) => {
-    const lat = pos.coords.latitude;
-    const lon = pos.coords.longitude;
-    const API_KEY = "7e18719ba0f1e53fde718c7e1b3e5454";
-
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-      )
-      .then(({ data }) => setWeather(data))
-      .catch((err) => console.log(err));
+  const [weather, setWeather] = useState();
+  const [currentBg, setCurrentBg] = useState("bg1");
+  const [textInput, setTextInput] = useState("");
+  const [finder, setFinder] = useState();
+  const [coords, setCoords] = useState()
+  
+  const success = position => {
+    const obj = {
+      lat: position.coords.latitude,
+      lon: position.coords.longitude,
+    }
+    setCoords(obj);
   };
+
+  useEffect(() => {
+    if (coords) {
+      const API_KEY = "7e18719ba0f1e53fde718c7e1b3e5454";
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${API_KEY}`
+      
+      axios
+      .get(url)
+      .then(({ data }) => setWeather(data))
+      .catch((err) => console.log(err))  
+    }
+  }, [coords]);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(success);
   }, []);
 
   useEffect(() => {
-    if(weather) {
-      setCurrentBg("bg2")
+    if (weather) {
+      setCurrentBg("bg2");
     }
-  }, [weather])
-  
+  }, [weather]);
+
+  useEffect(() => {
+    if (textInput) {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=${API_KEY}`;
+      axios
+        .get(url)
+        .then(({ data }) => setFinder(data.value))
+        .catch((err) => console.log(err));
+    }
+  }, [textInput]);
 
   return (
-    <main className={`${currentBg} font-["Lato"] flex justify-center items-center min-h-screen text-white px-2`}>
-      {weather === null ? (
+    <main
+      className={`${currentBg} font-["Lato"] flex justify-center items-center min-h-screen text-white px-2`}
+    >
+      {weather === undefined ? (
         <Loader />
+      ) : textInput ? (
+        <WeatherContainer 
+          weather={finder} 
+          setTextInput={setTextInput} 
+        />
       ) : (
-        <WeatherContainer weather={weather} />
+        <WeatherContainer 
+          weather={weather} 
+          setTextInput={setTextInput} 
+        />
       )}
     </main>
   );
--- src/components/WeatherContainer.jsx
+++ src/components/WeatherContainer.jsx
@@ -1,188 +1,101 @@
 /* eslint-disable react/no-unknown-property */
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./WeatherContainer.css";
 import WeatherStatus from "./WeatherStatus";
 
-const WeatherContainer = ({ weather }) => {
+const WeatherContainer = ({ weather, setTextInput }) => {
   const [isCelsius, setIsCelsius] = useState(true);
-  let weatherUrl
-
   const changeUnitTemp = (temp) => {
     if (isCelsius === true) {
       const celsiusTemp = (temp - 273.15).toFixed(1);
       return `${celsiusTemp}°C`;
     } else {
-      const fahrenheitTemp = ((temp - 273.15) * 9 / 5 + 32).toFixed(1);
+      const fahrenheitTemp = (((temp - 273.15) * 9) / 5 + 32).toFixed(1);
       return `${fahrenheitTemp}°F`;
     }
   };
 
   const handleChangeUnitTemp = () => {
     setIsCelsius(!isCelsius);
-  }
+  };
 
-  const weatherIcon = () => {
-    let weatherUrl 
-    weatherIcons.forEach(element => {
-      if (element.name === weather.weather[0].description) {
-        weatherUrl = element.url
-      }
-    });
-    return weatherUrl
-  }
+  const handleForm = (event) => {
+    event.preventDefault();
+    setTextInput(city.current.value.toLowerCase().trim());
+  };
 
-  
-  const weatherIcons = [
-    {
-      name: "snow",
-      url: "/snow.png"
-    },
-    {
-      name: "thunderstorm",
-      url: "/thunderstorm.png"
-    },
-    {
-      name: "rain",
-      url: "/rain.png" 
-    },
-    {
-      name: "shower rain",
-      url: "/shower_rain.png"
-    },
-    {
-      name: "broken clouds",
-      url: "/broken_clouds.png"
-    },
-    {
-      name: "scattered clouds",
-      url: "/scattered_clouds.png"
-    },
-    {
-      name: "few clouds",
-      url: "/few_clouds.png"
-    },
-    {
-      name: "clear sky",
-      url: "/clear_sky.png"
-    },
-    {
-      name: "mist",
-      url: "/mist.png"
-    },
-    {
-      name: "overcast clouds",
-      url: "/broken_clouds.png"
-    },
-  ]
+  const city = useRef();
 
   return (
-    <main className="min-h-screen grid ">
-      <span className="text-lg m-7">Weather app</span>
+    <main className="min-h-screen flex flex-col">
+  
+      <span className="font-extrabold text-2xl m-7 drop-shadow-2xl flex justify-center py-8">
+        Weather app
+      </span>
 
-      <article className="relative text-[#026EED]">
-        <picture className="m-2">
-          <svg
-            width="337"
-            height="231"
-            viewBox="0 0 419 285"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g filter="url(#filter0_d_702_1503)">
-              <path
-                d="M5 26.7381C5 9.83369 20.5103 -2.81603 37.0694 0.583238L392.669 73.581C405.086 76.1301 414 87.0593 414 99.7359V247.653C414 262.399 402.046 274.353 387.3 274.353H31.7003C16.9541 274.353 5 262.399 5 247.653V26.7381Z"
-                fill="url(#paint0_linear_702_1503)"
-              />
-            </g>
-            <defs>
-              <filter
-                id="filter0_d_702_1503"
-                x="0.145401"
-                y="0.0283203"
-                width="418.709"
-                height="284.034"
-                filterUnits="userSpaceOnUse"
-                color-interpolation-filters="sRGB"
-              >
-                <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                <feColorMatrix
-                  in="SourceAlpha"
-                  type="matrix"
-                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                  result="hardAlpha"
-                />
-                <feOffset dy="4.8546" />
-                <feGaussianBlur stdDeviation="2.4273" />
-                <feComposite in2="hardAlpha" operator="out" />
-                <feColorMatrix
-                  type="matrix"
-                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-                />
-                <feBlend
-                  mode="normal"
-                  in2="BackgroundImageFix"
-                  result="effect1_dropShadow_702_1503"
-                />
-                <feBlend
-                  mode="normal"
-                  in="SourceGraphic"
-                  in2="effect1_dropShadow_702_1503"
-                  result="shape"
-                />
-              </filter>
-              <linearGradient
-                id="paint0_linear_702_1503"
-                x1="5"
-                y1="123.225"
-                x2="414.028"
-                y2="118.181"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stop-color="#ebebeb" />
-                <stop offset="0.973958" stop-color="#bdecff" />
-              </linearGradient>
-            </defs>
-          </svg>
-        </picture>
-        <div className="absolute top-[35px] left-[10px] grid grid-cols-2 grid-rows-4 gap-x-[30px] pr-2 pl-1">
-          <span className="temp text-[40px] flex items-end">
-            {changeUnitTemp(weather.main.temp)}
-          </span>
-          <picture className="image flex items-center justify-center">
-            <img
-              src={`${weatherIcon()}`}
-              alt=""
-            />
-          </picture>
+      <form
+        className="flex flex-wrap items-center justify-center mb-11 py-4"
+        onSubmit={handleForm}
+      >
+        <input
+          className="text-slate-700 rounded-md"
+          type="text"
+          placeholder="City"
+          ref={city}
+        />
+        <button className="bg-[#38A1D8] mx-2 w-[110px] h-[28px] rounded-md shadow-lg shadow-gray-500 hover:bg-[#eef7fc] hover:text-[#38A1D8]">
+          Search
+        </button>
+      </form>
+
+
+      <article className="text-[#026EED] bg-[rgba(228,237,250,0.61)] flex items-center justify-around rounded-lg flex-col py-5">
+
+        <h2 className="location--container font-semibold text-2xl text-[#026EED]">
+          {weather?.name}, {weather?.sys.country}
+        </h2>
+        <span className="temp text-2xl flex items-end">
+          {changeUnitTemp(weather?.main.temp)}
+        </span>
+
+        <figure>
+          <img className="w-[120px] h-[120px]" src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} alt="" />
+        </figure>
+        <div className="font-medium">
+          <p className="desc--container flex items-center justify-center text-sm mr-1 font-semibold">
+            {weather?.weather[0].description.toUpperCase()}
+          </p>
           <div className="data--container flex flex-col justify-end font-normal">
             <WeatherStatus
               property="VIENTO"
               unit="m/s"
-              value={weather.wind.speed}
+              value={weather?.wind.speed}
             />
             <WeatherStatus
               property="NUBES"
               unit="%"
-              value={weather.clouds.all}
+              value={weather?.clouds.all}
             />
             <WeatherStatus
-              property="PRESION"
-              unit="hPa"
-              value={weather.main.pressure}
+              property="HUMEDAD"
+              unit="%"
+              value={weather?.main.humidity}
             />
           </div>
-          <p className="desc--container flex items-center justify-center text-sm mr-1 font-semibold">
-            {weather.weather[0].description.toUpperCase()}
-          </p>
-          <h3 className="location--container flex items-center font-semibold">
-            {weather.name}, {weather.sys.country}
-          </h3>
+        </div>
+        
+        <div className="flex ">
+          <button
+            className="bg-[#38A1D8] text-white m-auto w-[140px] h-[30px] rounded-md shadow-lg shadow-gray-500 hover:bg-[#eef7fc] hover:text-[#38A1D8]"
+            onClick={handleChangeUnitTemp}
+          >
+            {isCelsius ? "Cambiar a °F" : "Cambiar a °C"}
+          </button>
         </div>
       </article>
 
-      <div className="flex ">
-        <button className="bg-[#38A1D8] m-auto w-[140px] h-[30px] rounded-md shadow-lg shadow-gray-500 hover:bg-[#eef7fc] hover:text-[#38A1D8]" onClick={handleChangeUnitTemp}>{isCelsius ? "Cambiar a °F" : "Cambiar a °C"}</button>
-      </div>
+
+
     </main>
   );
 };
